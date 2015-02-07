65da02f52cb2a762e79a783c674d300688ebb196
Evan Sherwood <evan@neezer.net>
2015-02-07 12:14:02 -0800

    added asset prefix to hero block

--- src/components/blocks/HeroBlock.jsx
+++ src/components/blocks/HeroBlock.jsx
@@ -7,9 +7,13 @@ var HeroBlock = React.createClass({
     };
   },
 
+  getBgImageUrl: function() {
+    return [this.props.assetPrefix, this.props.data.bgImageSrc].join('');
+  },
+
   render: function() {
     return (
-      <div className={this.props.classNames.join(' ')} style={ { backgroundImage: "url(" + this.props.data.bgImageSrc + ")" } }>
+      <div className={this.props.classNames.join(' ')} style={ { backgroundImage: "url(" + this.getBgImageUrl() + ")" } }>
         <div>
           <h1>{this.props.data.heading}</h1>
           <p>{this.props.data.headingCopy}</p>
