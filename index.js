fb32f1563c2a6be661623453e281955483703cce
Evan Sherwood <evan@neezer.net>
2015-02-07 12:32:26 -0800

    adjusted crop on image; trying just exporting block selector

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-// digestable bits for admin
 module.exports.blockSelector = require('./src/components/blocks/blockSelector.js');
-module.exports.HeroBlock = require('./src/components/blocks/HeroBlock.jsx');
-module.exports.ListBlock = require('./src/components/blocks/ListBlock.jsx');
