657b067af14c922b9e16f9af0af2bc37d0282f95
Evan Sherwood <evan@neezer.net>
2015-02-09 13:07:50 -0800

    revised block selector

--- src/components/blocks/blockSelector.js
+++ src/components/blocks/blockSelector.js
@@ -1,13 +1,10 @@
 var HeroBlock   = require('./HeroBlock.jsx');
 var ListBlock   = require('./ListBlock.jsx');
-// var CTABlock
 
 module.exports = function(blockType) {
-  if (/^hero/.test(blockType)) {
-    var variant = (blockType.match(/^hero-(\d+)$/) || [])[1];
-    return { element: HeroBlock, variant: variant };
-  } else if (/^list/.test(blockType)) {
-    var variant = (blockType.match(/^list-(\d+)$/) || [])[1];
-    return { element: ListBlock, variant: variant };
+  switch(blockType) {
+    case "hero":  return HeroBlock;
+    case "list":  return ListBlock;
+    default:      return null;
   }
 };
