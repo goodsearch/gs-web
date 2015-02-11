fadfe7f56f3f0899227d5899da09febbe0a3a42e
Evan Sherwood <evan@neezer.net>
2015-02-10 17:17:53 -0800

    yet another possible fix

--- bin/build_production.sh
+++ bin/build_production.sh
@@ -1,5 +1,5 @@
-echo "Node environment set to: $NODE_ENV";
-if [ $NODE_ENV == 'production' ]
+echo "Node environment set to: $NODE_ENV"
+if [ "$NODE_ENV" == 'production' ]
 then
   ./node_modules/.bin/gulp build:production
 fi
