a12a3a67c5ed799cb6e7596d2cc15d885b836235
Evan Sherwood <evan@neezer.net>
2015-02-10 16:22:44 -0800

    updated test descriptions for hero block

--- test/components/blocks/HeroBlock-test.jsx
+++ test/components/blocks/HeroBlock-test.jsx
@@ -32,28 +32,28 @@ describe("HeroBlock component", function() {
     t.assert("url(" + blockData.bgImageSrc + ")" === this.componentNode.style["background-image"]);
   });
 
-  it("assigns the value for 'heading' to a <h1> tag", function() {
+  it("assigns data.heading to a <h1> tag", function() {
     this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
     var h1 = t.utils.findRenderedDOMComponentWithTag(this.component, "h1");
 
     t.assert(blockData.heading === h1.getDOMNode().textContent);
   });
 
-  it("assigns the value for 'headingCopy' to an <p> tag", function() {
+  it("assigns data.headingCopy to a <p> tag", function() {
     this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
     var p = t.utils.findRenderedDOMComponentWithTag(this.component, "p");
 
     t.assert(blockData.headingCopy === p.getDOMNode().textContent);
   });
 
-  it("assigns the value for 'ctaTarget' to an <a> tag's href", function() {
+  it("assigns data.ctaTarget to an <a> tag's href", function() {
     this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
     var a = t.utils.findRenderedDOMComponentWithTag(this.component, "a");
 
     t.assert(blockData.ctaTarget === a.getDOMNode().href);
   });
 
-  it("assigns the value for 'ctaCopy' to an <a> tag's content", function() {
+  it("assigns data.ctaCopy to an <a> tag's content", function() {
     this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
     var a = t.utils.findRenderedDOMComponentWithTag(this.component, "a");
 
@@ -61,7 +61,7 @@ describe("HeroBlock component", function() {
   });
 
   describe("when given an asset prefix", function() {
-    it("prepends the asset prefix to bgImageSrc", function() {
+    it("prepends the asset prefix to data.bgImageSrc", function() {
       var prefix = "/prefix";
 
       this.componentNode = t.utils.renderIntoDocument(
