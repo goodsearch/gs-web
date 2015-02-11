e405cf9218522605aeee410a5e956d680fb2b403
Evan Sherwood <evan@neezer.net>
2015-02-11 10:49:00 -0800

    gate stylus task for prod env

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -3,13 +3,18 @@ var stylus       = require('gulp-stylus');
 var config       = require('../config').stylus;
 var autoprefixer = require('gulp-autoprefixer');
 var gulpif       = require('gulp-if');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
+
+var production = process.env.NODE_ENV === 'production';
+
+if (!production) {
+  var browserSync = require('browser-sync');
+  var reload      = browserSync.reload;
+}
 
 gulp.task('stylus', function() {
   return gulp.src(config.src)
     .pipe(stylus(config.settings))
     .pipe(autoprefixer({ browsers: ['last 2 version'] }))
     .pipe(gulp.dest(config.dest))
-    .pipe(reload({ stream: true }));
+    .pipe(gulpif(!production, reload({ stream: true })));
 });
