3ac872b55c429c210cf19db73b8e17ebe6a355da
Evan Sherwood <evan@neezer.net>
2015-02-10 16:20:27 -0800

    hero block fully tested

--- test/components/blocks/HeroBlock-test.jsx
+++ test/components/blocks/HeroBlock-test.jsx
@@ -1,26 +1,74 @@
-var t         = require('../../react-test-helper.js');
-var React     = require('react');
+var t     = require("../../react-test-helper.js");
+var React = require("react");
+var _     = require("lodash");
 
 // SUT
-var HeroBlock = require('../../../src/components/blocks/HeroBlock.jsx');
+var HeroBlock = require("../../../src/components/blocks/HeroBlock.jsx");
 
-describe('HeroBlock component', function() {
-  before('render component, locate test handles', function() {
-    // FIXME: scaffold valid data elsewhere?
-    var blockData = {
-      bgImageSrc:   "bgImageSrc",
-      heading:      "heading",
-      headingCopy:  "headingCopy",
-      ctaTarget:    "ctaTarget",
-      ctaCopy:      "ctaCopy"
-    };
+var blockData = {
+  bgImageSrc:   "bgImageSrc",
+  heading:      "heading",
+  headingCopy:  "headingCopy",
+  ctaTarget:    "http://www.example.com/",
+  ctaCopy:      "ctaCopy"
+};
 
-    this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData} />);
-  });
-
+describe("HeroBlock component", function() {
   afterEach(t.clearDOM);
 
-  it('containing div should have the classes [block, hero]', function() {
-    t.assert('block hero' === this.component.getDOMNode().className);
+  it("container has the classes [block, hero]", function() {
+    this.componentNode = t.utils.renderIntoDocument(
+      <HeroBlock data={blockData}  />
+    ).getDOMNode();
+
+    t.assert("block hero" === this.componentNode.className);
+  });
+
+  it("container has the correct background image", function() {
+    this.componentNode = t.utils.renderIntoDocument(
+      <HeroBlock data={blockData}  />
+    ).getDOMNode();
+
+    t.assert("url(" + blockData.bgImageSrc + ")" === this.componentNode.style["background-image"]);
+  });
+
+  it("assigns the value for 'heading' to a <h1> tag", function() {
+    this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
+    var h1 = t.utils.findRenderedDOMComponentWithTag(this.component, "h1");
+
+    t.assert(blockData.heading === h1.getDOMNode().textContent);
+  });
+
+  it("assigns the value for 'headingCopy' to an <p> tag", function() {
+    this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
+    var p = t.utils.findRenderedDOMComponentWithTag(this.component, "p");
+
+    t.assert(blockData.headingCopy === p.getDOMNode().textContent);
+  });
+
+  it("assigns the value for 'ctaTarget' to an <a> tag's href", function() {
+    this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
+    var a = t.utils.findRenderedDOMComponentWithTag(this.component, "a");
+
+    t.assert(blockData.ctaTarget === a.getDOMNode().href);
+  });
+
+  it("assigns the value for 'ctaCopy' to an <a> tag's content", function() {
+    this.component = t.utils.renderIntoDocument(<HeroBlock data={blockData}  />);
+    var a = t.utils.findRenderedDOMComponentWithTag(this.component, "a");
+
+    t.assert(blockData.ctaCopy === a.getDOMNode().textContent);
+  });
+
+  describe("when given an asset prefix", function() {
+    it("prepends the asset prefix to bgImageSrc", function() {
+      var prefix = "/prefix";
+
+      this.componentNode = t.utils.renderIntoDocument(
+        <HeroBlock data={blockData} assetPrefix={prefix} />
+      ).getDOMNode();
+
+      t.assert("url(" + prefix + blockData.bgImageSrc + ")" === this.componentNode.style["background-image"]);
+    });
   });
 });
